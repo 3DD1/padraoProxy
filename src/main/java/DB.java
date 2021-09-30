f16e917e2a7949b097feadd8ec97f0c4a852a58c
Edmilson <scovinowork@gmail.com>
2021-09-29 22:48:42 -0300

    up

---
+++ src/main/java/DB.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+public class DB {
+    private static Map<Integer>, Athlete> athlete = new HastMap<>();
+
+    public static Athlete getAthlete(Integer cadastro){
+        return athlete.get(matricula);
+    }
+    public static void  addAthlete
+}
