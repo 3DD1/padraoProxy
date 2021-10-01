fcff5b5bcb8ab524d05dca9b3486b9bfb685197c
Edmilson <scovinowork@gmail.com>
2021-09-30 23:43:14 -0300

    up

---
+++ src/main/java/Athele.java
@@ -0,0 +1,22 @@
+public class Athele implements IAthlete{
+
+    private Integer matricula;
+    private String primeiroNome;
+    private String segundoNome;
+    private String treino;
+    private PersonalTrainer personal;
+
+    public Athele(int matricula) {
+        this.matricula = matricula;
+        Athlete
+        this.primeiroNome = primeiroNome;
+        this.segundoNome = segundoNome;
+        this.treino = treino;
+        this.personal = personal;
+
+    }
+
+
+
+
+}
--- src/main/java/BD.java
+++ src/main/java/BD.java
@@ -1,3 +1,12 @@
+import java.util.Map;
+
 public class BD {
-    private static
+    private static Map<Integer>, Athlete> athletes = new HastMap<>();
+
+    public static Athlete getAthlete(Integer registration){
+        return athletes.get(matricula);
+    }
+    public static void  addAthlete(Athlete athlete){
+        athletes.put(athlete.getMatricula(), athlete);
+    }
 }
